2359396bd04ebd2e04cc96902f76187b43a5cae2
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-12-08 14:21:09 -0500

    do part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -23,15 +23,16 @@ enum LeftRight {
     Right,
 }
 
-type ThreeChars = [char; 3];
+type ThreeChars = [u8; 3];
 fn string_to_three_chars(s: &str) -> ThreeChars {
     let mut chars = s.chars();
     [
-        chars.next().unwrap(),
-        chars.next().unwrap(),
-        chars.next().unwrap(),
+        chars.next().unwrap() as u8,
+        chars.next().unwrap() as u8,
+        chars.next().unwrap() as u8,
     ]
 }
+const Z_AS_U8: u8 = 'Z' as u8;
 #[derive(Debug)]
 struct Node {
     pub name: ThreeChars,
@@ -47,7 +48,7 @@ struct Input {
 }
 
 const PART_1_EXPECTED_TEST_OUTPUT: Output = 6;
-const PART_2_EXPECTED_TEST_OUTPUT: Output = 5905;
+const PART_2_EXPECTED_TEST_OUTPUT: Output = 6;
 
 fn parse_input(input: &str) -> IResult<&str, Input> {
     let parse_left = || map(tag("L"), |_| LeftRight::Left);
@@ -91,7 +92,7 @@ fn solve_part1(input: Input) -> Output {
     });
 
     let mut visited = 0;
-    let mut current_node = ['A', 'A', 'A'];
+    let mut current_node = ['A' as u8, 'A' as u8, 'A' as u8];
 
     for direction in input.sequence.iter().cloned().cycle() {
         let node = nodes.get(&current_node).unwrap();
@@ -102,7 +103,7 @@ fn solve_part1(input: Input) -> Output {
 
         visited += 1;
 
-        if current_node[0] == 'Z' && current_node[1] == 'Z' && current_node[2] == 'Z' {
+        if current_node[0] == Z_AS_U8 && current_node[1] == Z_AS_U8 && current_node[2] == Z_AS_U8 {
             break;
         }
     }
@@ -111,7 +112,41 @@ fn solve_part1(input: Input) -> Output {
 }
 
 fn solve_part2(input: Input) -> Output {
-    todo!();
+    let nodes = input.nodes.iter().fold(HashMap::new(), |mut acc, node| {
+        acc.insert(node.name.clone(), node);
+        acc
+    });
+
+    let mut visited = 0;
+    let mut current_nodes = input
+        .nodes
+        .iter()
+        .filter(|node| node.name[2] == 'A' as u8)
+        .map(|node| node.name.clone())
+        .collect::<Vec<_>>();
+
+    let distances = current_nodes.iter().map(|node| {
+        let mut count = 0;
+        let mut current = node;
+
+        for direction in input.sequence.iter().cloned().cycle() {
+            count += 1;
+            let node = nodes.get(current).unwrap();
+            current = match direction {
+                LeftRight::Left => &node.left,
+                LeftRight::Right => &node.right,
+            };
+            if current[2] == Z_AS_U8 {
+                break;
+            }
+        }
+
+        return count;
+    });
+
+    println!("current_nodes: {:?}", current_nodes.len());
+
+    distances.fold(1, |acc, x| num::integer::lcm(acc, x))
 }
 
 fn main() {
@@ -119,8 +154,8 @@ fn main() {
     println!("Part 1: {:?}", solve_part1(input));
 
     // lol
-    // let input = parse_input(include_str!("../real-input.txt")).unwrap().1;
-    // println!("Part 2: {:?}", solve_part2(input));
+    let input = parse_input(include_str!("../real-input.txt")).unwrap().1;
+    println!("Part 2: {:?}", solve_part2(input));
 }
 
 #[cfg(test)]
@@ -138,11 +173,11 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn part2() {
-    //     assert_eq!(
-    //         solve_part2(parse_input(include_str!("../test-input-2.txt")).unwrap().1),
-    //         PART_2_EXPECTED_TEST_OUTPUT
-    //     );
-    // }
+    #[test]
+    fn part2() {
+        assert_eq!(
+            solve_part2(parse_input(include_str!("../test-input-2.txt")).unwrap().1),
+            PART_2_EXPECTED_TEST_OUTPUT
+        );
+    }
 }
